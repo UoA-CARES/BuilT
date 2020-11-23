14a8de8045098a9b6e267acb2dbdd67473c4fc67
jongyoon <seedengine@gmail.com>
2020-11-24 00:46:07 +1300

    bert download, added tweet classification logger

--- tweet/download_data.sh
+++ tweet/download_data.sh
@@ -3,9 +3,11 @@
 export KaggleCompName=tweet-sentiment-extraction
 export DownloadPath=input
 export RobertaBase=roberta-base
+export BertBaseUncased=bert-base-uncased
 
 kaggle competitions download -c $KaggleCompName -p $DownloadPath
 
 unzip $DownloadPath/$KaggleCompName.zip -d $DownloadPath/$KaggleCompName
 
 kaggle datasets download abhishek/$RobertaBase -p $DownloadPath/$RobertaBase --unzip
+kaggle datasets download abhishek/$BertBaseUncased -p $DownloadPath/$BertBaseUncased --unzip
---
+++ tweet/tweet_logger.py
@@ -0,0 +1,37 @@
+
+import logging
+import torch
+import numpy as np
+
+from built.logger import LoggerBase
+from built.registry import Registry
+
+
+@Registry.register(category="hooks")
+class TweetLogger(LoggerBase):
+    def __call__(self, writer, split, outputs, labels, log_dict,
+                 epoch, step=None, num_steps_in_epoch=None):
+
+        if step is not None:
+            assert num_steps_in_epoch is not None
+            log_step = epoch * 10000 + (step / num_steps_in_epoch) * 10000
+            log_step = int(log_step)
+        else:
+            log_step = epoch
+
+        for key, value in log_dict.items():
+            if self.use_tensorboard:
+                writer['tensorboard'].add_scalar(
+                    f'{split}/{key}', value, log_step)
+
+        if self.use_wandb:
+            log_dict.update({'epoch': epoch, 'mode': split})
+            writer['wandb'].log(log_dict)
+        
+        if labels is not None and outputs is not None:
+            labels = labels['sentiment_target']
+            outputs = torch.sigmoid(outputs).cpu().detach().numpy().tolist()
+            outputs = np.argmax(outputs, axis=1)
+        
+            writer['tensorboard'].add_pr_curve(
+                f'pr_curve_{split}', labels, outputs, log_step)
