82eb2db7f3653fc3cf5409b5d93c83ef0b0ff638
Inkyu <inkyu.sa@csiro.au>
2021-01-12 15:33:40 +1000

    pipeline test (sc->se-es -> se-esc) and (se-es -> se-esc)

--- pipeline_inference_es_esc.sh
+++ pipeline_inference_es_esc.sh
@@ -4,20 +4,20 @@ export TOKENIZERS_PARALLELISM=true
 DEVICE_ID=0
 
 #CONFIG=tweet/config/tweet_sentiment_classification.yaml
-CONFIG=tweet/config/for_sensors/14.[TR_CORR]_[SC]_[ROB].yaml
-CUDA_VISIBLE_DEVICES=$DEVICE_ID python run.py ensemble_sentiment with $CONFIG \
-  ensemble_train=False \
-  ensemble_split='test' \
-  ensemble_csv_input_path='tweet/input/tweet-sentiment-extraction/corrected_new_test.csv' \
-  ensemble_csv_output_path='tweet/input/tweet-sentiment-extraction/corrected_new_test_Sentiment.csv' \
-  -f
+#CONFIG=tweet/config/for_sensors/14.[TR_CORR]_[SC]_[ROB].yaml
+#CUDA_VISIBLE_DEVICES=$DEVICE_ID python run.py ensemble_sentiment with $CONFIG \
+#  ensemble_train=False \
+#  ensemble_split='test' \
+#  ensemble_csv_input_path='tweet/input/tweet-sentiment-extraction/corrected_new_test.csv' \
+#  ensemble_csv_output_path='tweet/input/tweet-sentiment-extraction/corrected_new_test_Sentiment.csv' \
+#  -f
 
 
 CONFIG=tweet/config/for_sensors/20.[TR_CORR]_[SE]_[Es]_[ROB].yaml
 CUDA_VISIBLE_DEVICES=$DEVICE_ID python run.py ensemble_index_extraction with $CONFIG \
   ensemble_train=False \
   ensemble_split='test' \
-  ensemble_csv_input_path='tweet/input/tweet-sentiment-extraction/corrected_new_test_Sentiment.csv' \
+  ensemble_csv_input_path='tweet/input/tweet-sentiment-extraction/corrected_new_test.csv' \
   ensemble_csv_output_path='tweet/input/tweet-sentiment-extraction/corrected_new_test_Sentiment_Index.csv' \
   original_selected_text_column='selected_text' \
   -f
