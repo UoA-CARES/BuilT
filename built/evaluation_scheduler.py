7aeba2f66364e9caaa050edd1adb773d0b58fd7e
jongyoon <seedengine@gmail.com>
2021-08-24 16:40:45 +1200

    fix eval scheduler

--- built/evaluation_scheduler.py
+++ built/evaluation_scheduler.py
@@ -3,7 +3,7 @@ class EvaluationScheduler(object):
     def __init__(self, boundary_scores, intervals):
         self.boundary_scores = boundary_scores
         self.intervals = intervals
-        self.last_eval_step = 0
+        self.last_eval_step = -1
         self.__total_step = None
         self.current_eval_interval = None
 
@@ -11,20 +11,23 @@ class EvaluationScheduler(object):
         assert(self.current_eval_interval is not None)
         
         # step starts from 0, so we need to add 1
-        return step + 1 >= self.last_eval_step + self.current_eval_interval
+        return step >= self.last_eval_step + self.current_eval_interval
 
     def update(self, step, score):
         assert(self.__total_step is not None)
+        self.last_eval_step = step
+        
         for bs, interval in zip(self.boundary_scores, self.intervals):
             if score >= bs:
-                # if interval < self.current_eval_interval:
                 if interval == 0:
                     self.current_eval_interval = self.total_step
-                else:
+                elif interval < self.current_eval_interval:
                     self.current_eval_interval = interval
-                self.last_eval_step = step
-                # print(f'interval={self.current_eval_interval}, last_eval_step={self.last_eval_step}')
-                break
+                else:
+                    # do nothing, keep the current interval
+                    pass
+                
+        # print(f'interval={self.current_eval_interval}, last_eval_step={self.last_eval_step}')
 
     @property
     def total_step(self) -> int:
@@ -33,10 +36,15 @@ class EvaluationScheduler(object):
     @total_step.setter
     def total_step(self, total_step: int):
         self.__total_step = total_step
-        if self.intervals[0] == 0:
+        
+        if self.current_eval_interval is None and self.intervals[0] == 0:
             self.current_eval_interval = self.__total_step
-        else:
+        elif self.current_eval_interval is None:
             self.current_eval_interval = self.intervals[0]
+        else:
+            pass
+
+        
 
         
 
